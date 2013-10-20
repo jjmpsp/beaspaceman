88ca8cb881ab8a19e4c074436efeedf36ff1314d
Joel Murphy <360hex@googlemail.com>
2013-10-20 12:07:36 +0100

    Database built

--- application/models/jobs.php
+++ application/models/jobs.php
@@ -7,6 +7,11 @@
             parent::__construct();
         }
         
+        function add_job($dbdata)
+        {
+            $query = $this->db->insert('job', $dbdata);
+        }
+
         function get_last_ten_entries()
         {
             $query = $this->db->get('job', 10);
@@ -16,6 +21,7 @@
         function jobs_by_name($name)
         {
             $this->db->like('name', $name);
+            $this->db->order_by('name','asc');
             $query = $this->db->get('job');
             return $query->result();
         }
